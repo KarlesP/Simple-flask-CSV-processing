aca0032126d7723e9e20f9cfbd758abe00c36bb0
Mr Koios <george.nostradamos@gmail.com>
2018-07-30 13:59:21 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -35,7 +35,9 @@ def results():
     # Handle form submissions. Can only receive POST requests.
     if request.method == 'POST':
         io_table=pd.read_csv(request.files['io_table'])
-        factor=pd.read_csv(request.files['factor'])  
+        io_t=io_table
+        factor=pd.read_csv(request.files['factor'])
+        f=factor
         #Calculate row sum and column sum
         c_sum = io_table.sum(axis=0)
         r_sum = io_table.sum(axis=1)
@@ -51,7 +53,19 @@ def results():
         #Connect the factor column to the rest of the table
         io_table=pd.concat([io_table, factor], axis=1)
         io=io_table
+        mdl2=pd.DataFrame(io_t.values*f.values, columns=io_t.columns, index=io_t.index)
+        mdl1=pd.DataFrame(io_t.values+f.values, columns=io_t.columns, index=io_t.index)
+        #option = request.form['model']
+        #if option == "mdl1":
+            #mdl_t="yes"
+            #mdl_t=pd.DataFrame(io_t.values+f.values, columns=io_t.columns, index=io_t.index)
+        #elif option == "mdl2":
+            #mdl_t=
+            #mdl_t=pd.DataFrame(io_t.values*f.values, columns=io_t.columns, index=io_t.index)
+        #elif option == "mdl2" and option== "mdl1":
+            #mdl1_t=pd.DataFrame(io_t.values+f.values, columns=io_t.columns, index=io_t.index)
+            #mdl2_t=pd.DataFrame(io_t.values*f.values, columns=io_t.columns, index=io_t.index) 
         #results = new_io_table(io_table,factor) # Get some results
-    return render_template('/results.html', io=io.to_html())
+    return render_template('/results.html', io=io.to_html(), mdl1=mdl1.to_html(),mdl2=mdl2.to_html())
 if __name__ == "__main__":
     app.run(debug=True)
